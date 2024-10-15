c2500e653c167720a549f59a38455bebee2b0bce
rmorag2102 <rmorag@ucenfotec.ac.cr>
2024-10-14 22:00:22 -0600

    Agregando submenu busqueda

--- main.cpp
+++ main.cpp
@@ -97,7 +97,7 @@ public:
       T value1 = _getTerminalInput<T>();
       U value2 = _getTerminalInput<U>();
       this->_operate(value1, value2, this->_dictionary);
-    });   
+    });
   }
 };
 
@@ -306,6 +306,69 @@ TerminalApp getSubmenuCategorias()
   return submenuCategorias;
 }
 
+TerminalApp getSubmenuBusqueda()
+{
+  auto buscarNombre = UnaryOperation<int>()
+    .withDictionary({
+      {"prompt", "Buscar por Nombre: "},
+      {"description", "Busqueda por nombre"},
+      {"result", "Buscando nombre...\n"},
+      {"error", "Error al buscar.\n"}
+    })
+    .onInput([](int nombre, const auto &dict) {
+      _print(dict.at("result"));
+    });
+
+    auto buscarCategoria = UnaryOperation<int>()
+    .withDictionary({
+      {"prompt", "Buscar por Categoría: "},
+      {"description", "Busqueda por Categoría"},
+      {"result", "Buscando categoria...\n"},
+      {"error", "Error al buscar.\n"}
+    })
+    .onInput([](int id, const auto &dict) {
+      _print(dict.at("result"));
+    });
+
+    auto buscarAtributos = UnaryOperation<int>()
+    .withDictionary({
+      {"prompt", "Buscar por Atributos: "},
+      {"description", "Busqueda por Atributos"},
+      {"result", "Buscando Atributos...\n"},
+      {"error", "Error al buscar.\n"}
+    })
+    .onInput([](int id, const auto &dict) {
+      _print(dict.at("result"));
+    });
+
+    auto buscarRangoPrecios = UnaryOperation<int>()
+    .withDictionary({
+      {"prompt", "Buscar por Rango de Precios: "},
+      {"description", "Busqueda por Rango de Precios"},
+      {"result", "Buscando Rango de Precios...\n"},
+      {"error", "Error al buscar.\n"}
+    })
+    .onInput([](int id, const auto &dict) {
+      _print(dict.at("result"));
+    });
+
+    auto submenuBusqueda = TerminalApp::Builder()
+    .withDictionary({
+      {"title", "Proyecto!"},
+      {"description", "Submenu Busqueda"},
+      {"prompt", "Elija una opcion: "},
+      {"exit", "Saliendo...\n"},
+      {"invalid_option", "Opcion invalida, por favor intentalo de nuevo.\n"}
+    })
+    .withOperation<UnaryOperation<int>>(buscarNombre)
+    .withOperation<UnaryOperation<int>>(buscarCategoria)
+    .withOperation<UnaryOperation<int>>(buscarAtributos)
+    .withOperation<UnaryOperation<int>>(buscarRangoPrecios)
+    .build();
+
+  return submenuBusqueda;
+}
+
 
 
 TerminalApp GetAppInstance()
@@ -319,6 +382,7 @@ TerminalApp GetAppInstance()
     })
     .withOperation<TerminalApp>(getSubmenuProductos())
     .withOperation<TerminalApp>(getSubmenuCategorias())
+    .withOperation<TerminalApp>(getSubmenuBusqueda())
     .build();
 
 
